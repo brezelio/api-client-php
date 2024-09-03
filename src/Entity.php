3be561e10504adda13867445f6f58f30949df15d
Keanu Buschbacher <flynamicobserver@gmail.com>
2024-09-03 15:53:05 +0200

    fix: remove deprecated setting of dynamic properties

--- src/Entity.php
+++ src/Entity.php
@@ -13,6 +13,7 @@ class Entity implements ArrayAccess
     public int $module_id;
     public ?string $created_at = null;
     public ?string $updated_at = null;
+    public ?array $attributes = [];
 
     /**
      * @throws ReflectionException
@@ -22,7 +23,7 @@ class Entity implements ArrayAccess
         $this->id = $attributes['id'];
         $this->module_id = $attributes['module_id'];
         foreach ($attributes as $key => $value) {
-            $this->$key = $this->parseProperty($key, $value);
+            $this->offsetSet($key, $this->parseProperty($key, $value));
         }
     }
 
@@ -44,28 +45,56 @@ class Entity implements ArrayAccess
         return $value;
     }
 
+    /**
+     * Convert the entity to an array.
+     *
+     * @return array
+     */
     public function toArray(): array
     {
-        return get_object_vars($this);
+        $attributes = array_merge($this->attributes, get_object_vars($this));
+        foreach ($attributes as $key => $value) {
+            $attributes[$key] = is_object($value) && method_exists($value, 'toArray') ? $value->toArray() : $value;
+            if ($key === 'attributes') {
+                unset($attributes[$key]);
+            }
+        }
+        return array_merge(
+            [
+                'id' => $this->id,
+                'module_id' => $this->module_id,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ],
+            $attributes
+        );
     }
 
     public function offsetExists(mixed $offset): bool
     {
-        return property_exists($this, $offset);
+        return property_exists($this, $offset) || array_key_exists($offset, $this->attributes);
     }
 
     public function offsetGet(mixed $offset): mixed
     {
-        return $this->$offset;
+        return $this->$offset ?? $this->attributes[$offset] ?? null;
     }
 
     public function offsetSet(mixed $offset, mixed $value): void
     {
-        $this->$offset = $value;
+        if (property_exists($this, $offset)) {
+            $this->$offset = $value;
+        } else {
+            $this->attributes[$offset] = $value;
+        }
     }
 
     public function offsetUnset(mixed $offset): void
     {
-        unset($this->$offset);
+        if (property_exists($this, $offset)) {
+            $this->$offset = null;
+        } else {
+            unset($this->attributes[$offset]);
+        }
     }
 }
